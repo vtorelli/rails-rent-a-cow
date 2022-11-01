7376b0c4732158d1230d3afb40029a0ae66e76a2
Nicolas Blanchard <blanchard.nicolas@hotmail.com>
2022-11-01 18:16:52 +0100

    change cows index, add cloudinary images

--- Gemfile
+++ Gemfile
@@ -47,6 +47,9 @@ gem "bootsnap", require: false
 # Use Sass to process CSS
 gem "sassc-rails"
 
+# Use cloudinary external cloud
+gem "cloudinary"
+
 # Use Active Storage variants [https://guides.rubyonrails.org/active_storage_overview.html#transforming-images]
 # gem "image_processing", "~> 1.2"
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,6 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+require "open-uri"
+
+file = URI.open("https://upload.wikimedia.org/wikipedia/commons/thumb/8/82/NES-Console-Set.jpg/1200px-NES-Console-Set.jpg")
+cow = Cow.new(name: "NES", description: "A great console", price_per_day: 200, image_url: "http://myimage.com")
+cow.photo.attach(io: file, filename: "nes.png", content_type: "image/png")
+cow.save
