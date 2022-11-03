04363e8e159dba4cb41054e7ad005362b35a0341
Nicolas Blanchard <blanchard.nicolas@hotmail.com>
2022-11-03 13:26:34 +0100

    Add depency in the user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
-  has_many :bookings
-  has_many :cow
+  has_many :bookings, dependent: :destroy
+  has_many :cows, dependent: :destroy
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
