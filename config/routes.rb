2d481c33c32cc14f90b0052502282ffc851471d1
Taraneh Karimi <tarahkari@gmail.com>
2022-11-03 16:01:43 -0400

    create booking route and controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  resources :cows, only: [:index, :new, :create, :show, :edit, :update]
+  resources :cows, only: [:index, :new, :create, :show, :edit, :update] do
+    resources :bookings, only: [:new, :create]
+  end
 
+  resources :bookings, only: [:index, :show, :edit, :update]
   devise_for :users
   root to: "pages#home"
 end
