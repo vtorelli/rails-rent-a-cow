e7b44ef1d839745103c8fbbffd3ffa74b2ab5eef
Nicolas Blanchard <blanchard.nicolas@hotmail.com>
2022-11-08 20:55:54 +0100

    add location

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_06_064039) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_08_194528) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -71,6 +71,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_06_064039) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id", null: false
+    t.string "location"
     t.index ["user_id"], name: "index_cows_on_user_id"
   end
 
