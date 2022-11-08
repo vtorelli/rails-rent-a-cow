e5db9d23ce2943e5d6d9a14df0e02dd2fae29500
Nicolas Blanchard <blanchard.nicolas@hotmail.com>
2022-11-08 20:11:33 +0100

    add search bar

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,16 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
 
   def home
-    @cows = Cow.all
+    if params[:query].present?
+      sql_query = <<~SQL
+        cows.name @@ :query
+        OR cows.description @@ :query
+        OR tags.name @@ :query
+      SQL
+      @cows = Cow.joins(:tags).where(sql_query, query: "%#{params[:query]}%")
+      @cows = @cows.uniq
+    else
+      @cows = Cow.all
+    end
   end
 end
