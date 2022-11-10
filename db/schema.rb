abe17b62adc83385117097bd10012bf2830f09c9
Rishabh Agnihotri <agnihotri.cotidiano@gmail.com>
2022-11-10 22:35:54 +0400

    tag add funstionality

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema[7.0].define(version: 2022_11_06_063024) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_08_194528) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
