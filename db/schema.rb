0fcdb817773e99d0cba52c022a3472f82608651d
Victoria Torelli <v.torelli@icloud.com>
2022-11-12 12:10:42 +0100

    styled main map

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_11_161330) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_11_120849) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -67,6 +67,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_11_161330) do
     t.string "name"
     t.text "description"
     t.integer "price_per_day"
+    t.string "image_url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id", null: false
